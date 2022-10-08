071d2e524e5a47cc51677aa356c4244ca42a83c6
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-10-08 09:22:35 +0530

    fg

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AddUser from './Components/AddUser';
+import UserDashboard from './Components/UserDashboard';
 
 function App() {
   return (
     <div className="App">
       <AddUser />
+      <UserDashboard />
     </div>
   );
 }
